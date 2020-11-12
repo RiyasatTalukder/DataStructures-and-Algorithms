9da4d89abaf129d56286dae569f3383336e27747
riyasat <riyasattalukder@gmail.com>
2020-11-11 22:53:44 -0500

    Added the node definition for the double linked list

--- DataStructures/LRUCache_java.java
+++ DataStructures/LRUCache_java.java
@@ -1,5 +1,11 @@
 public class LRUCache_java {
     private int capacity;
+    public class DLNode {
+        int key;
+        int value;
+        DLNode pre;
+        DLNode next;
+    }
     public LRUCache_java(int capacity) {
         this.capacity = capacity;
     }
