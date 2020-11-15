cde77899016cab45f6756c12bc5a0c752c51aa67
riyasat <riyasattalukder@gmail.com>
2020-11-14 21:52:11 -0500

    Added the code for left rotate without considering bf

--- DataStructures/AVLTree_py.py
+++ DataStructures/AVLTree_py.py
@@ -1,12 +1,12 @@
 class Node:
     key = 0
+    parent = None
     left = None
     right = None
     balance_factor = 0
 
 class AVLTree:
-    root = None
-
+    root = Node
     def search(self, root, key):
         if(key == root.key):
             return root
@@ -14,6 +14,30 @@ class AVLTree:
             return self.search(root.left, key)
         else:
             return self.search(root.right, key)
+    
+    def leftRotate(self, root):
+        #left subtree of pivot becomes right subtree of root, pivot becomes new root
+
+        #make right subtree of root = left subtree of pivot
+        pivot = root.right
+        root.right = pivot.left
+        #re-assign the parents accordingly
+        if(pivot.left != None):
+            pivot.left.parent = root
+        pivot.parent = root.parent
+        if(root.parent == None):
+            self.root = pivot
+        elif(root.parent.left == root):
+            root.parent.left = pivot
+        else:
+            root.parent.right = pivot
+
+        pivot.left = root
+        root.parent = pivot
+        #update balance factors
+        
+        return pivot
+
 
 
 
