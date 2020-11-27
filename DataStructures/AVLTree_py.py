525fa5805520e5223e41e211a92cdcc319bc6cab
riyasat <riyasattalukder@gmail.com>
2020-11-26 21:11:52 -0500

    added the cases for insertion and updated document.

--- DataStructures/AVLTree_py.py
+++ DataStructures/AVLTree_py.py
@@ -1,3 +1,10 @@
+"""
+
+
+----IN PROGRESS---
+
+
+"""
 class Node:
     key = 0
     parent = None
@@ -22,23 +29,23 @@ class AVLTree:
         - Child refers to the parent of the inserted node
 
         1) left-left
-            In this case, the left side of the parent and its child is heavy
+            In this case, the left side of the parent and its left child is heavy
             We can perform a RIGHT rotation on the parent node to balance the tree
 
         2) left-right
-            Left side of the parent is heavy and the right side of the child is heavy
+            Left side of the parent is heavy and the right side of the left child is heavy
             We can perform a LEFT rotation on the child and then RIGHT rotation on parent 
 
         3) right-right
-            The right side of the parent and its child is heavy 
+            The right side of the parent and its right child is heavy 
             We can perform a LEFT rotation on the parent node to balance the tree
 
         4) right-left
-            The right of the parent is heavy and the left side of the child is heavy
+            The right of the parent is heavy and the left side of the right child is heavy
             We can perform a RIGHT rotation on the child and then LEFT rotation on parent
 
         """
-        #To do
+        #IN PROGRESS (NOT DONE)
         if(root == None):
             return node
         if(root.value < node.value):
@@ -47,7 +54,35 @@ class AVLTree:
             root = self.insert(root.left, node)
         
         node.bf = 0
+        i = node.parent
+        if(node == i.right):
+            i.bf+=1
+        else:
+            i.bf-=1
         
+        if(i.bf == 0):
+            return root
+        
+        #case 1
+        if(i.bf == -2 and i.left.bf == -1):
+            self.rightRotate(i)
+        #case 2
+        if(i.bf == -2 and i.left.bf == 1):
+            self.leftRotate(i.left)
+            self.rightRotate(i)
+        #case 3
+        if(i.bf == 2 and i.right.bf == 1):
+            self.leftRotate(i)
+        #case 4
+        if(i.bf == 2 and i.right.bf == -1):
+            self.rightRotate(i.right)
+            self.leftRotate(i)
+        
+        if(i == root):
+            return root
+        
+        i = i.parent
+
         return root
 
     def delete(self, root, node):
