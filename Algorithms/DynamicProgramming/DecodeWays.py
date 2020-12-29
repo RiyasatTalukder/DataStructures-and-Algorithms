86129b7078d1ab9a1cffb03d9a32ec54501bc0c1
riyasat <riyasattalukder@gmail.com>
2020-12-29 01:18:20 -0500

    added the finished code and documentation for the decode ways problem.

--- Algorithms/DynamicProgramming/DecodeWays.py
+++ Algorithms/DynamicProgramming/DecodeWays.py
@@ -1,19 +1,33 @@
 def numDecodings(s):
+    """
+    Given a string of numbers, the algorithm decodes
+    the string and returns the number of possible messages
+    that can be decoded. The decoding is defined as:
+    '1' -> 'A', '2' -> 'B' ..... '26' -> 'Z'
+
+    Time: O(n)
+    Space: O(n)
+    """
     if(len(s) == 0):
         return 0
-    if(len(s) == 1):
-        return 1 if s[0] != '0' else 0
-    if('00' in s[:2]):
-        return 0
-    
-    dp = [0 for i in range(len(s)+1)]
+    #initialize dp array
+    dp = [0 for _ in range(len(s)+1)]
+    #base cases
+    dp[0] = 1
     dp[1] = 1 if s[0] != '0' else 0
-    dp[2] = 2 if '0' not in s[:2] and int(s[:2]) <= 26 else 1
-    
-    for i in range(3, len(dp)):
-        if(s[i-1] != '0'):
-            dp[i] = dp[i-1] + dp[i-2]
-    
-    print(dp)
+
+    #dp approach: we have the possibility of decoding 1 character or 2 characters
+    #if we decide to decode 1 character, we add the number of ways to decode the i-1 charcters, if ith character is valid
+    #Similarly, for 2 characters, we add the number of ways to decode the i-2 charcters, if i-2:i characters are valid
+    #We add these together to account for both possibilities
+
+    for i in range(2, len(dp)):
+        if(int(s[i-1:i]) > 0):
+            dp[i]+=dp[i-1]
+        if(10 <= int(s[i-2:i]) <= 26):
+            dp[i]+=dp[i-2]
     return dp[-1]
-    
+
+s = '11111'
+assert 8 == numDecodings(s)
+
