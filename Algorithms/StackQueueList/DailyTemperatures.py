9a971e0673f5a666bc21786b1b0dcb98c9405e59
riyasat <riyasattalukder@gmail.com>
2023-02-02 00:21:13 -0500

    added a stack question

--- Algorithms/StackQueueList/DailyTemperatures.py
+++ Algorithms/StackQueueList/DailyTemperatures.py
@@ -17,3 +17,6 @@ def dailyTemperatures(temperatures):
         stack.append([i, temperatures[i]])
     
     return result
+
+temperatures = [73,74,75,71,69,72,76,73]
+assert(dailyTemperatures(temperatures) == [1,1,4,2,1,1,0,0])
