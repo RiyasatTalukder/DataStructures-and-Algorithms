3b94034227b7f762cff3af936dc1861515f9cc64
riyasat <riyasattalukder@gmail.com>
2023-02-02 00:19:35 -0500

    added a stack question

---
+++ Algorithms/StackQueueList/DailyTemperatures.py
@@ -0,0 +1,19 @@
+'''
+Given an array of daily temperatures, the algorithm returns the number of days
+till the next warmer temperature from each record.
+
+Time: O(n)
+Space: O(n)
+'''
+
+def dailyTemperatures(temperatures):
+    # stack to store temperatures and pop them if matching pair (warmer) is found
+    stack, result = [], [0] * len(temperatures)
+
+    for i in range(len(temperatures)):
+        while stack and stack[-1][1] < temperatures[i]:
+            prev_temp = stack.pop()
+            result[prev_temp[0]] = i - prev_temp[0]
+        stack.append([i, temperatures[i]])
+    
+    return result
